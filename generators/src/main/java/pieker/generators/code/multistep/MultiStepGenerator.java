f1e8f5f2cbd94dd5c4cc74ffe26e9585c613a12e
Simon Ohlsen <stu216738@mail.uni-kiel.de>
2025-03-28 14:43:15 +0100

    enhance JarBuilder and MultiStepGenerator

--- generators/src/main/java/pieker/generators/code/multistep/MultiStepGenerator.java
+++ generators/src/main/java/pieker/generators/code/multistep/MultiStepGenerator.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.velocity.VelocityContext;
 import pieker.common.ScenarioComponent;
 import pieker.common.ScenarioTestPlan;
+import pieker.generators.code.CodeGenerationException;
 import pieker.generators.code.VelocityTemplateProcessor;
 import pieker.generators.util.FileSystemUtils;
 import pieker.generators.util.JarBuilder;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -20,22 +22,42 @@ import java.util.Map;
 public class MultiStepGenerator {
 
     private static final String EXECUTION_NAME = System.getProperty("executionName");
-    private static final String PROJECT_ROOT = System.getProperty("projectRoot", "../../../../../../../");
+    private static final String PROJECT_ROOT = System.getProperty("projectRoot");
     private static final String GEN_DIR = PROJECT_ROOT + System.getProperty("genDir", "../.gen/");
-    private static final String CODE_DIR = GEN_DIR + EXECUTION_NAME + "/code/";
-    private static final String PROXY_TEMPLATE = "multistep/MultiStepProxy.vm";
-    private static final String MANIFEST_TEMPLATE = "multistep/MANIFEST.vm";
+    private static final String CODE_DIR = GEN_DIR + EXECUTION_NAME + File.separator + "code" + File.separator;
+    private static final String PROXY_TEMPLATE = "multistep" + File.separator + "MultiStepProxy.vm";
+    private static final String MANIFEST_TEMPLATE = "multistep" + File.separator + "MANIFEST.vm";
     private static final VelocityTemplateProcessor TEMPLATE_PROCESSOR = new VelocityTemplateProcessor();
 
-    public static void generateMultiStepProxies(ScenarioTestPlan testPlan) throws IOException, InterruptedException {
+    /**
+     * Generates multi-step proxies for the given test plan based on generated Java files for each step.
+     *
+     * @param testPlan test plan
+     * @throws CodeGenerationException if an error occurs
+     */
+    public static void generateMultiStepProxies(ScenarioTestPlan testPlan) throws CodeGenerationException {
         for (ScenarioComponent component : testPlan.getComponents()) {
-            generateMultiStepProxy(component.getName());
+            try {
+                generateMultiStepProxy(component.getName());
+            } catch (IOException | InterruptedException e) {
+                log.error("Error generating multi-step proxy for component '{}'", component.getName(), e);
+                throw new CodeGenerationException(e.getMessage());
+            }
         }
     }
 
+    /**
+     * Generates a multi-step proxy for the given component based on generated Java files for each step.
+     *
+     * @param componentName name of the component
+     * @throws IOException          if an error occurs
+     * @throws InterruptedException if the process is interrupted
+     */
     public static void generateMultiStepProxy(String componentName) throws IOException, InterruptedException {
         final List<String> javaStepFiles = FileSystemUtils.getFiles(CODE_DIR + componentName, ".java");
         final List<String> fileNames = FileSystemUtils.getFileNames(javaStepFiles);
+        final String componentDir = CODE_DIR + componentName + File.separator;
+        final String componentJarDir = componentDir + "jars" + File.separator;
 
         log.debug("Found {} Java files for component '{}': {}", javaStepFiles.size(), componentName, javaStepFiles);
 
@@ -45,6 +67,7 @@ public class MultiStepGenerator {
         }
 
         // Create MultiStepProxy.java file for current component
+        final String multiStepProxyName = componentName.replace('-', '_');
         VelocityContext proxyContext = new VelocityContext();
         proxyContext.put("port", 42690);
         Map<String, String> endpointJars = new HashMap<>();
@@ -54,17 +77,16 @@ public class MultiStepGenerator {
         }
         proxyContext.put("endpoints", endpointJars.keySet());
         proxyContext.put("endpointJars", endpointJars);
-        TEMPLATE_PROCESSOR.processTemplate(PROXY_TEMPLATE, proxyContext, componentName + ".java",
-                CODE_DIR + componentName + "/");
+        proxyContext.put("proxyName", multiStepProxyName);
+        TEMPLATE_PROCESSOR.processTemplate(PROXY_TEMPLATE, proxyContext, multiStepProxyName + ".java", componentDir);
 
         // Create MANIFEST.MF for Multistep Proxy JAR
         VelocityContext manifestContext = new VelocityContext();
+        manifestContext.put("proxyName", multiStepProxyName);
         manifestContext.put("jars", fileNames.stream().map(name -> name.replace("java", "jar")).toList());
-        TEMPLATE_PROCESSOR.processTemplate(MANIFEST_TEMPLATE, manifestContext, "MANIFEST.MF",
-                CODE_DIR + componentName + "/");
+        TEMPLATE_PROCESSOR.processTemplate(MANIFEST_TEMPLATE, manifestContext, "MANIFEST.MF", componentJarDir);
 
         // Build Multistep JAR
-        //TODO
-
+        JarBuilder.buildJar(componentDir + multiStepProxyName + ".java", componentDir + "jars", true);
     }
 }
--- generators/src/main/java/pieker/generators/util/JarBuilder.java
+++ generators/src/main/java/pieker/generators/util/JarBuilder.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.Objects;
 
 @Slf4j
 public final class JarBuilder {
@@ -21,11 +21,56 @@ public final class JarBuilder {
      * @throws InterruptedException if the process is interrupted
      */
     public static void buildJar(String javaFilePath) throws IOException, InterruptedException {
-        // Get the directory of the .java file
         File javaFile = new File(javaFilePath);
         String directory = javaFile.getParent();
         String buildDirPath = directory + File.separator + "build";
         String outputDirPath = directory + File.separator + "jars";
+        buildJar(javaFilePath, buildDirPath, outputDirPath);
+    }
+
+    /**
+     * Builds a JAR file from a given Java file.
+     *
+     * @param javaFilePath path to the Java file
+     * @param buildDirPath path to the build directory
+     * @param skipManifest whether to skip creating a manifest file
+     * @throws IOException          if an error occurs
+     * @throws InterruptedException if the process is interrupted
+     */
+    public static void buildJar(String javaFilePath, String buildDirPath, boolean skipManifest) throws IOException, InterruptedException {
+        File javaFile = new File(javaFilePath);
+        String directory = javaFile.getParent();
+        String outputDirPath = directory + File.separator + "jars";
+        buildJar(javaFilePath, buildDirPath, outputDirPath, skipManifest);
+    }
+
+    /**
+     * Builds a JAR file from a given Java file.
+     *
+     * @param javaFilePath  path to the Java file
+     * @param buildDirPath  path to the build directory
+     * @param outputDirPath path to the output directory
+     * @throws IOException          if an error occurs
+     * @throws InterruptedException if the process is interrupted
+     */
+    public static void buildJar(String javaFilePath, String buildDirPath, String outputDirPath) throws IOException, InterruptedException {
+        buildJar(javaFilePath, buildDirPath, outputDirPath, false);
+    }
+
+    /**
+     * Builds a JAR file from a given Java file.
+     *
+     * @param javaFilePath  path to the Java file
+     * @param buildDirPath  path to the build directory
+     * @param outputDirPath path to the output directory
+     * @param skipManifest  whether to skip creating a manifest file
+     * @throws IOException          if an error occurs
+     * @throws InterruptedException if the process is interrupted
+     */
+    public static void buildJar(String javaFilePath, String buildDirPath, String outputDirPath, boolean skipManifest) throws IOException, InterruptedException {
+        // Get the directory of the .java file
+        File javaFile = new File(javaFilePath);
+        String directory = javaFile.getParent();
 
         // Create the build directory
         File buildDir = new File(buildDirPath);
@@ -43,11 +88,13 @@ public final class JarBuilder {
             throw new IOException("Failed to compile " + javaFilePath);
         }
 
-        // Create a manifest file for the JAR
         File manifestFile = new File(buildDirPath, "MANIFEST.MF");
         String mainClassName = javaFile.getName().replace(".java", "");
-        String manifestContent = "Manifest-Version: 1.0\nMain-Class: " + mainClassName + "\n";
-        Files.write(manifestFile.toPath(), manifestContent.getBytes());
+        // Create a manifest file for the JAR
+        if (!skipManifest) {
+            String manifestContent = "Manifest-Version: 1.0\nMain-Class: " + mainClassName + "\n";
+            Files.write(manifestFile.toPath(), manifestContent.getBytes());
+        }
 
         // Build the JAR file
         File jarFile = new File(outputDirPath, mainClassName + ".jar");
@@ -66,14 +113,15 @@ public final class JarBuilder {
         deleteClassFiles(buildDirPath);
     }
 
-    public static void buildMultiStateProxyJar() {
-        // TODO
-    }
-
+    /**
+     * Deletes all .class files in the given directory.
+     *
+     * @param directoryPath path to the directory
+     */
     private static void deleteClassFiles(String directoryPath) {
         File dir = new File(directoryPath);
         if (dir.exists() && dir.isDirectory()) {
-            for (File file : dir.listFiles((d, name) -> name.endsWith(".class"))) {
+            for (File file : Objects.requireNonNull(dir.listFiles((d, name) -> name.endsWith(".class")))) {
                 if (!file.delete()) {
                     log.error("Failed to delete file: {}", file.getAbsolutePath());
                 }
