b4f0f0b06d517342a10969ec73de8668a6ed1d50
YIllmann <y.illmann@yahoo.com>
2025-05-07 12:36:26 +0200

    implement concurrent evaluation

---
+++ evaluator/src/test/java/pieker/evaluator/EvaluatorTest.java
@@ -0,0 +1,43 @@
+package pieker.evaluator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import pieker.api.Assertions;
+import pieker.api.assertions.StubAssert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+class EvaluatorTest {
+
+    List<Assertions> assertionList = new ArrayList<>();
+
+    @Test
+    void testEvaluator() {
+        Evaluator evaluator = new Evaluator();
+        createAssertionList();
+        evaluator.run(this.assertionList, System.currentTimeMillis(), 60);
+        this.assertionList.forEach(ass -> {
+            ass.getEvaluation().forEach(evaluation -> assertTrue(evaluation.isSuccess()));
+        });
+    }
+
+    void createAssertionList(){
+        StubAssert stubAssert = new StubAssert("Assert A");
+        stubAssert.setAssertAfter(3);
+        this.assertionList.add(stubAssert);
+        StubAssert stubAssert2 = new StubAssert("Assert B");
+        stubAssert2.setAssertAfter(1);
+        this.assertionList.add(stubAssert2);
+        StubAssert stubAssert3 = new StubAssert("Assert C");
+        stubAssert3.setAssertAfter(7);
+        this.assertionList.add(stubAssert3);
+        StubAssert stubAssert4 = new StubAssert("Assert D");
+        stubAssert4.setAssertAfter(5);
+        this.assertionList.add(stubAssert4);
+    }
+
+}
