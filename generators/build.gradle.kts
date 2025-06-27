ab30f590b457f7ed25e67c4a145571c1e8dcdea9
Simon Ohlsen <stu216738@mail.uni-kiel.de>
2025-06-27 12:12:53 +0200

    update docker-java and JUnit dependencies

--- generators/build.gradle.kts
+++ generators/build.gradle.kts
@@ -10,6 +10,6 @@ dependencies {
     //External
     implementation("org.apache.velocity:velocity-engine-core:2.4.1")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.19.1")
-    implementation("com.github.docker-java:docker-java:3.5.1")
-    implementation("com.github.docker-java:docker-java-transport-httpclient5:3.5.1")
+    implementation("com.github.docker-java:docker-java:3.5.2")
+    implementation("com.github.docker-java:docker-java-transport-httpclient5:3.5.2")
 }
