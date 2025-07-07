0e91e86a193bd412cf9f7b70110f3183b68584fe
Simon Ohlsen <stu216738@mail.uni-kiel.de>
2025-07-07 22:51:09 +0200

    add support for networks and volumes in Compose

--- architectures/src/main/java/pieker/architectures/compose/model/ComposeNetwork.java
+++ architectures/src/main/java/pieker/architectures/compose/model/ComposeNetwork.java
@@ -2,21 +2,31 @@ package pieker.architectures.compose.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import pieker.architectures.model.CompositeNetwork;
 import pieker.architectures.model.Network;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static pieker.architectures.util.MapUtils.putIfNotNullOrEmpty;
+
 /**
  * Represents a Docker Compose network in the Docker Compose architecture model.
- * TODO: Add full support
  */
 @Getter
 @Setter
 public class ComposeNetwork extends Network implements ComposeComponent {
 
     private String driver;
+    private Object driverOpts = new HashMap<>();
+    private boolean attachable;
+    private boolean enableIpv4;
+    private boolean enableIpv6;
+    private boolean external;
+    private Object ipam;
+    private boolean internal;
+    private Object labels;
+    private String name;
+
 
     public ComposeNetwork(String name) {
         this.setComponentName(name);
@@ -30,9 +40,16 @@ public class ComposeNetwork extends Network implements ComposeComponent {
     @Override
     public Map<String, Object> toMap() {
         Map<String, Object> attributeMap = new HashMap<>();
-
-        attributeMap.put("driver", this.driver);
-
-        return Map.of(this.getName(), attributeMap);
+        putIfNotNullOrEmpty(attributeMap, "driver", this.getDriver());
+        putIfNotNullOrEmpty(attributeMap, "driver_opts", this.getDriverOpts());
+        putIfNotNullOrEmpty(attributeMap, "attachable", this.isAttachable());
+        putIfNotNullOrEmpty(attributeMap, "enable_ipv4", this.isEnableIpv4());
+        putIfNotNullOrEmpty(attributeMap, "enable_ipv6", this.isEnableIpv6());
+        putIfNotNullOrEmpty(attributeMap, "external", this.isExternal());
+        putIfNotNullOrEmpty(attributeMap, "ipam", this.getIpam());
+        putIfNotNullOrEmpty(attributeMap, "internal", this.isInternal());
+        putIfNotNullOrEmpty(attributeMap, "labels", this.getLabels());
+        putIfNotNullOrEmpty(attributeMap, "name", this.getName());
+        return attributeMap;
     }
 }
--- architectures/src/main/java/pieker/architectures/compose/model/ComposeVolume.java
+++ architectures/src/main/java/pieker/architectures/compose/model/ComposeVolume.java
@@ -3,20 +3,26 @@ package pieker.architectures.compose.model;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import pieker.architectures.model.CompositeStorage;
 import pieker.architectures.model.Storage;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static pieker.architectures.util.MapUtils.mapToList;
+import static pieker.architectures.util.MapUtils.putIfNotNullOrEmpty;
+
 /**
  * Represents a Docker Compose volume in the Docker Compose architecture model.
- * <p>
- * TODO: Add full support
  */
 @Getter
 @Setter
 public class ComposeVolume extends Storage implements ComposeComponent {
+
+    private String driver;
+    private Map<String, String> driverOpts;
+    private boolean external;
+    private Map<String, String> labels = new HashMap<>();
+    private String name;
 
     @Builder
     public ComposeVolume(String name) {
@@ -27,7 +33,11 @@ public class ComposeVolume extends Storage implements ComposeComponent {
     @Override
     public Map<String, Object> toMap() {
         Map<String, Object> attributeMap = new HashMap<>();
-
-        return Map.of(this.getName(), attributeMap);
+        putIfNotNullOrEmpty(attributeMap, "driver", this.getDriver());
+        putIfNotNullOrEmpty(attributeMap, "driver_opts", mapToList(this.getDriverOpts()));
+        putIfNotNullOrEmpty(attributeMap, "external", this.isExternal());
+        putIfNotNullOrEmpty(attributeMap, "labels", mapToList(this.getLabels()));
+        putIfNotNullOrEmpty(attributeMap, "name", this.getName());
+        return attributeMap;
     }
 }
