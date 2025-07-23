63417baa92ccb93889a6ff0106abf28534fcabc0
Simon Ohlsen <stu216738@mail.uni-kiel.de>
2025-07-23 14:05:02 +0200

    update Jackson and JUnit dependencies

--- evaluator/build.gradle.kts
+++ evaluator/build.gradle.kts
@@ -7,5 +7,5 @@ dependencies {
     implementation(project(":architectures"))
     implementation(project(":common"))
     implementation("org.json:json:20250517")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.19.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.19.2")
 }
