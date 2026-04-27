5f6536f53ba93902c5114869416ffbd745e5022a
Simon Ohlsen <100684567+simohlsen@users.noreply.github.com>
2026-04-27 18:40:39 +0200

    Pump Java, Gradle, and Libs (#63)
    * update java, gradle, and libs
    
    * update build pipeline
    
    * update web server to java 25

--- dsl/build.gradle.kts
+++ dsl/build.gradle.kts
@@ -10,14 +10,14 @@ dependencies {
     // External
     implementation("org.antlr:antlr4-runtime:4.13.2")
     implementation("org.cornutum.regexp:regexp-gen:2.0.1")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.19.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.21.2")
     implementation("com.github.jsqlparser:jsqlparser:5.3")
     implementation("org.apache.velocity:velocity-engine-core:2.4.1")
     implementation("org.apache.commons:commons-jexl3:3.5.0")
     implementation("org.json:json:20250517")
     implementation("info.picocli:picocli:4.7.7")
 
-    testImplementation("org.postgresql:postgresql:42.7.7")
-    testImplementation("org.testcontainers:junit-jupiter:1.21.3")
+    testImplementation("org.postgresql:postgresql:42.7.10")
+    testImplementation("org.testcontainers:junit-jupiter:1.21.4")
     testImplementation("org.testcontainers:postgresql:1.21.3")
 }
